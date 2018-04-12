f7f7004605e2784f6781bd3588e8afa8392e7a0a
SundayPlayer <baptiste.vade@sundayplayer.fr>
2018-04-12 09:18:10 +0200

    add Task relation in UserEntity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -18,6 +18,29 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var Task[]|\Doctrine\Common\Collections\ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="Task", mappedBy="users")
+     */
+    private $tasks;
+
+    /**
+     * @return Task[]|\Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+
+    /**
+     * @param Task[]|\Doctrine\Common\Collections\ArrayCollection $tasks
+     */
+    public function setTasks($tasks)
+    {
+        $this->tasks = $tasks;
+    }
+
     public function __construct()
     {
         parent::__construct();
